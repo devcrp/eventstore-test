0f3c06b3e93c7d5c3c7c1ea2e296c8db0ed18ccc
devcrp <devcrp>
2020-06-19 10:21:44 +0200

    Testing Blazor + EventStore

---
+++ src/AtmMachine/AtmMachine/Shared/Dtos/AccountCreationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtmMachine.Shared.Dtos
+{
+    public class AccountCreationDto
+    {
+        public decimal InitialDeposit { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
